d1f24e29029c5de9daefe29068190bc907ea21a3
Marcus Wetterbro <wetterbro@gmail.com>
2023-10-09 18:38:21 +0200

    Remove Nodejs

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 
-// impoterar JSON filen och sparar den.
-//impoterar "File system" modulen 
-let fs = require("fs");
-const fromJSON = fs.readFileSync("./test.json")
-const dishesData = JSON.parse(fromJSON);
-
-/*
-const selectedDish = dishesData[1]; 
-dishesData.forEach(element => {
-    if (element.vegan === true) {
-        console.log(element.disheName);
-        console.log("This dish is vegan.");
-    } else {
-        console.log(element.disheName);
-        console.log("This dish is not vegan.");
-    }  
-});
-*/
+async function fetchJsonData() {
+    try {
+      const response = await fetch("index.json");
+      const dishesData = await response.json();
+  
+      dishesData.forEach(element => {
+        if (element.vegan === true) {
+          console.log(element.disheName);
+          console.log("This dish is vegan.");
+        } else {
+          console.log(element.disheName);
+          console.log("This dish is not vegan.");
+        }
+      });
+    } catch (error) {
+      console.error("An error occurred:", error);
+    }
+  }
+  
+  fetchJsonData();
 
