72546398a2eaa33ca36682082adee806afa31e33
dennan141 <dennis.karlsson96@outlook.com>
2023-10-09 20:58:38 +0200

    Added vegan finding function

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
-//Imports data to the foodData object as an object
+//Imports data to the foodData object as an object from the JSON file
 import foodData from "./index.json" assert { type: "json" };
 
 
 
+
+
+
+
+
+/* ------ Filter functions ------- */
+
 //returns an array with all vegan food items
-//TODO 
 function getAllVegan() {
   const veganDishArray = [];
 
+
+  for (let index = 0; index < foodData.length; index++) {
+    const currentFood = foodData[index]
+    if (currentFood.vegan === true) {
+      veganDishArray.push(currentFood)
+    }
+  }
+
+  return veganDishArray
 
 }
 
