c00304117e95ed69c34680a90eefc74981d29e16
dennan141 <dennis.karlsson96@outlook.com>
2023-10-09 20:41:17 +0200

    Changed JSON data loading, now returns as object

--- script.js
+++ script.js
@@ -1,22 +1,13 @@
+//Imports data to the foodData object as an object
+import foodData from "./index.json" assert { type: "json" };
 
-async function fetchJsonData() {
-    try {
-      const response = await fetch("index.json");
-      const dishesData = await response.json();
-  
-      dishesData.forEach(element => {
-        if (element.vegan === true) {
-          console.log(element.disheName);
-          console.log("This dish is vegan.");
-        } else {
-          console.log(element.disheName);
-          console.log("This dish is not vegan.");
-        }
-      });
-    } catch (error) {
-      console.error("An error occurred:", error);
-    }
-  }
-  
-  fetchJsonData();
+
+
+//returns an array with all vegan food items
+//TODO 
+function getAllVegan() {
+  const veganDishArray = [];
+
+
+}
 
