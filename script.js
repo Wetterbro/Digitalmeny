c025b137324d07e74145207c19b5a8f81607c511
Marcus Wetterbro <wetterbro@gmail.com>
2023-10-09 14:32:14 +0200

    Added Nodejs and updated js and json file

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 
+// impoterar JSON filen och sparar den.
+//impoterar "File system" modulen 
+let fs = require("fs");
+const fromJSON = fs.readFileSync("./test.json")
+const dishesData = JSON.parse(fromJSON);
+
+/*
+const selectedDish = dishesData[1]; 
+dishesData.forEach(element => {
+    if (element.vegan === true) {
+        console.log(element.disheName);
+        console.log("This dish is vegan.");
+    } else {
+        console.log(element.disheName);
+        console.log("This dish is not vegan.");
+    }  
+});
+*/
+
