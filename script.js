8dbaa72de5fe2f53265d4edbd3e1d2c1ebf05d4d
Marcus Wetterbro <wetterbro@gmail.com>
2023-10-18 20:22:46 +0200

    fixed translation for muti buttons

--- script.js
+++ script.js
@@ -326,11 +326,11 @@ function createMenuCard(dish) {
     // If a half-price is defined, display it along with the regular price
     menuDetailsHTML = `
     <h2>${dish.disheName[foodDataLangSelect]} <img src="./assets/img/half_full.png" alt="half circle">
-     ${dish.price[0]}kr <img src="./assets/img/full.png" alt="full circle"> ${dish.price[1]}kr
+     ${dish.price[0]}kr <img src="./assets/img/full.png" alt="full circle"> ${dish.price[0]}kr
      ${dish.vegan ? '<img src="./assets/img/vegan.png" alt="Vegan icon" class="float-end>' : ''}</h2>
     <p>${dish.about[foodDataLangSelect]}</p>
-    <button class="btn btn-outline-primary" data-translate="addToCartHalf" data-price="${dish.price[0]}">Lägg till i varukorgen (Halv)</button>
-    <button class="btn btn-outline-primary" data-translate="addToCartFull" data-price="${dish.price[1]}">Lägg till i varukorgen (Hel)</button>
+    <button class="btn btn-outline-primary" data-translate="addToCartHalf" data-price="${dish.price[1]}">Lägg till i varukorgen (Halv)</button>
+    <button class="btn btn-outline-primary" data-translate="addToCartFull" data-price="${dish.price[0]}">Lägg till i varukorgen (Hel)</button>
   `;
   } else {
     // If no half-price is defined, display the regular price
